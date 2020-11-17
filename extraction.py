e180ae8840f4e5fc248a04be7810e649fabede4d
Leslie Le <lel23@mail.wlu.edu>
2020-11-17 00:18:05 -0500

    Organized code
    Signed-off-by: Leslie Le <lel23@mail.wlu.edu>

--- extraction.py
+++ extraction.py
@@ -41,13 +41,10 @@ feat_labels = X.columns
 
 ## PLOTTING ##
 def plot_decision_regions(X, y, classifier, resolution=0.02):
-
-    # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
-    # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
@@ -58,7 +55,6 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
-    # plot examples by class
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0], 
                     y=X[y == cl, 1],
@@ -71,180 +67,115 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
 ## PCA ##
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
                                                     stratify=y,
-                                                  random_state=0)
+                                                    random_state=0)
 
 stdsc = StandardScaler()
 X_train_std = stdsc.fit_transform(X_train)
 X_test_std = stdsc.transform(X_test)
 
+def pca_selection():
+    cov_mat = np.cov(X_train_std.T)
+    eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+    print('\nEigenvalues \n%s' % eigen_vals)
+    print('\nEigenvectors \n%s' % eigen_vecs)
 
-cov_mat = np.cov(X_train_std.T)
-eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
-print('\nEigenvalues \n%s' % eigen_vals)
-print('\nEigenvectors \n%s' % eigen_vecs)
+    tot = sum(eigen_vals)
+    var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+    cum_var_exp = np.cumsum(var_exp)
+    plt.bar(range(0, len(df.columns) -1 ), var_exp, alpha=0.5, align='center',    
+            label='Individual explained variance')
+    plt.step(range(0, len(df.columns) -1 ), cum_var_exp, where='mid',         
+             label='Cumulative explained variance')
+    plt.ylabel('Explained variance ratio')
+    plt.xlabel('Principal component index')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    # plt.savefig('images/05_02.png', dpi=300)plt.show()
+    plt.show()
 
+    eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) 
+                   for i in range(len(eigen_vals))]
+    # Sort the (eigenvalue, eigenvector) tuples from high to low
+    eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+    w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+                   eigen_pairs[1][1][:, np.newaxis]))
+    print('Matrix W:\n', w)
 
-tot = sum(eigen_vals)
-var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
-cum_var_exp = np.cumsum(var_exp)
-plt.bar(range(0, len(df.columns) -1 ), var_exp, alpha=0.5, align='center',    
-        label='Individual explained variance')
-plt.step(range(0, len(df.columns) -1 ), cum_var_exp, where='mid',         
-         label='Cumulative explained variance')
-plt.ylabel('Explained variance ratio')
-plt.xlabel('Principal component index')
-plt.legend(loc='best')
-plt.tight_layout()
-# plt.savefig('images/05_02.png', dpi=300)plt.show()
-plt.show()
+    X_train_std[0].dot(w)
+    X_train_pca = X_train_std.dot(w)
+    colors = ['r', 'b', 'g']
+    markers = ['s', 'x', 'o']
+    for l, c, m in zip(np.unique(y_train), colors, markers):
+        plt.scatter(X_train_pca[y_train == l, 0],
+                    X_train_pca[y_train == l, 1],
+                    c=c, label=l, marker=m)
+        
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    plt.legend(loc='lower left')
+    plt.tight_layout()
+    # plt.savefig('images/05_03.png', dpi=300)
+    plt.show()
 
-eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i]) 
-               for i in range(len(eigen_vals))]
-# Sort the (eigenvalue, eigenvector) tuples from high to low
-eigen_pairs.sort(key=lambda k: k[0], reverse=True)
-w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
-               eigen_pairs[1][1][:, np.newaxis]))
-print('Matrix W:\n', w)
-X_train_std[0].dot(w)
-X_train_pca = X_train_std.dot(w)
-colors = ['r', 'b', 'g']
-markers = ['s', 'x', 'o']
-for l, c, m in zip(np.unique(y_train), colors, markers):
-    plt.scatter(X_train_pca[y_train == l, 0],
-                 X_train_pca[y_train == l, 1],
-                 c=c, label=l, marker=m)
-plt.xlabel('PC 1')
-plt.ylabel('PC 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-# plt.savefig('images/05_03.png', dpi=300)
-plt.show()
+def pca():
+    pca = PCA(n_components = 2)
+    lr = LogisticRegression(multi_class = 'ovr',
+                            random_state = 1,
+                            solver = 'lbfgs')
 
-'''
-pca = PCA(n_components = 2)
-lr = LogisticRegression(multi_class = 'ovr',
-                        random_state = 1,
-                        solver = 'lbfgs')
+    X_train_pca = pca.fit_transform(X_train_std)
+    X_test_pca = pca.transform(X_test_std)
+    lr.fit(X_train_pca, y_train)
+    plot_decision_regions(X_train_pca, y_train, classifier = lr)
 
-X_train_pca = pca.fit_transform(X_train_std)
-X_test_pca = pca.transform(X_test_std)
-lr.fit(X_train_pca, y_train)
-plot_decision_regions(X_train_pca, y_train, classifier = lr)
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    plt.legend(loc='lower left')
+    plt.tight_layout()
+    # plt.savefig('images/05_03.png', dpi=300)
+    plt.show()
 
-plt.xlabel('PC 1')
-plt.ylabel('PC 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-# plt.savefig('images/05_03.png', dpi=300)
-plt.show()
+def kpca():
+    kpca = KernelPCA(n_components = 2)
+    lr2 = LogisticRegression(multi_class = 'ovr',
+                             random_state = 1,
+                             solver = 'lbfgs')
 
-from sklearn.decomposition import KernelPCA
-kpca = KernelPCA(n_components = 2)
-lr2 = LogisticRegression(multi_class = 'ovr',
-                        random_state = 1,
-                        solver = 'lbfgs')
+    X_train_kpca = kpca.fit_transform(X_train_std)
+    lr2.fit(X_train_kpca, y_train)
+    plot_decision_regions(X_train_kpca, y_train, classifier = lr2)
 
-X_train_kpca = kpca.fit_transform(X_train_std)
-X_test_kpca = kpca.transform(X_test_std)
-lr2.fit(X_train_kpca, y_train)
-plot_decision_regions(X_train_kpca, y_train, classifier = lr2)
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    plt.legend(loc='lower left')
+    plt.tight_layout()
+    # plt.savefig('images/05_03.png', dpi=300)
+    plt.show()
 
-plt.xlabel('PC 1')
-plt.ylabel('PC 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-# plt.savefig('images/05_03.png', dpi=300)
-plt.show()
+def lda():
+    # solver = [''svd, lsqr, eigen]
+    lda = LDA(n_components=2)
+    X_train_lda = lda.fit_transform(X_train_std, y_train)
 
+    lr = LogisticRegression(multi_class='ovr', random_state=1, solver='lbfgs')
+    lr = lr.fit(X_train_lda, y_train)
 
-'''
-
-
-## LDA ##
-lda = LDA(n_components=2)
-X_train_lda = lda.fit_transform(X_train_std, y_train)
-lr = LogisticRegression(multi_class='ovr', random_state=1, solver='lbfgs')
-lr = lr.fit(X_train_lda, y_train)
-plot_decision_regions(X_train_lda, y_train, classifier=lr)
-plt.xlabel('LD 1')
-plt.ylabel('LD 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-plt.savefig('lda_train.png', dpi=300)
-plt.show()
-
-
-X_test_lda = lda.transform(X_test_std)
-plot_decision_regions(X_test_lda, y_test, classifier=lr)
-plt.xlabel('LD 1')
-plt.ylabel('LD 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-# plt.savefig('lda_test.png', dpi=300)
-plt.show()
-'''
-
-stdsc = Normalizer()
-X_train_std = stdsc.fit_transform(X_train)
-X_test_std = stdsc.fit_transform(X_test)
-feat_labels = X.columns
+    plot_decision_regions(X_train_lda, y_train, classifier=lr)
+    plt.xlabel('LD 1')
+    plt.ylabel('LD 2')
+    plt.legend(loc='lower left')
+    plt.tight_layout()
+    plt.savefig('lda_train.png', dpi=300)
+    plt.show()
 
 
-forest = RandomForestClassifier(n_estimators=500,
-                                random_state=1)
+    X_test_lda = lda.transform(X_test_std)
+    plot_decision_regions(X_test_lda, y_test, classifier=lr)
+    plt.xlabel('LD 1')
+    plt.ylabel('LD 2')
+    plt.legend(loc='lower left')
+    plt.tight_layout()
+    # plt.savefig('lda_test.png', dpi=300)
+    plt.show()
 
-forest.fit(X, y)
-importances = forest.feature_importances_
-
-indices = np.argsort(importances)[::-1]
-
-for f in range(X.shape[1]):
-    print("%2d) %-*s %f" % (f + 1, 30, 
-                            feat_labels[indices[f]], 
-                            importances[indices[f]]))
-
-sfm = SelectFromModel(forest, prefit=True)
-X_selected = sfm.transform(X)
-print('Number of features that meet this threshold criterion:', 
-      X_selected.shape[1])
-print("Threshold %f" % np.mean(importances))
-
-
-# Now, let's print the  features that met the threshold criterion for feature selection that we set earlier (note that this code snippet does not appear in the actual book but was added to this notebook later for illustrative purposes):
-cols = []
-for f in range(X_selected.shape[1]):
-    cols.append(feat_labels[indices[f]])    
-    print("%2d) %-*s %f" % (f + 1, 30, 
-                            feat_labels[indices[f]], 
-                            importances[indices[f]]))
-
-X_train_std = X_train_std[:, :X_selected.shape[1]]
-X_test_std = X_test_std[: , :X_selected.shape[1]]
-
-lda = LDA(n_components=2)
-X_train_lda = lda.fit_transform(X_train_std, y_train)
-
-
-
-lr = LogisticRegression(multi_class='ovr', random_state=1, solver='lbfgs')
-lr = lr.fit(X_train_lda, y_train)
-
-plot_decision_regions(X_train_lda, y_train, classifier=lr)
-plt.xlabel('LD 1')
-plt.ylabel('LD 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-plt.savefig('lda_train.png', dpi=300)
-plt.show()
-
-
-X_test_lda = lda.transform(X_test_std)
-
-plot_decision_regions(X_test_lda, y_test, classifier=lr)
-plt.xlabel('LD 1')
-plt.ylabel('LD 2')
-plt.legend(loc='lower left')
-plt.tight_layout()
-plt.savefig('lda_test.png', dpi=300)
-plt.show()
-'''
+pca()
