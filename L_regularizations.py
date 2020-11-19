f69a72acc2922516cc89051e64845b80c1d969b8
JosephSalerno <56265826+JosephSalerno@users.noreply.github.com>
2020-11-19 14:39:58 -0500

    works

--- L_regularizations.py
+++ L_regularizations.py
@@ -15,17 +15,43 @@ from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import GridSearchCV
 from sklearn.utils import check_random_state
 from sklearn import datasets
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Reading data
+df = pd.read_csv('NewBioDegWCols.csv')
+df.columns = ['SpMax_L','J_Dz','nHM','F01','F04','NssssC','nCb-','C%','nCp',
+              'n0','F03CN','SdssC','HyWi_B','LOC','SM6_L','F03CO','Me','Mi',
+              'nN-N','nArN02','nCRX3','SpPosA_B','nCIR','B01','B03','N-073',
+              'SpMax_A','Psi_i_1d','B04','Sd0','TI2_L','nCrt','c-026','F02',
+              'nHDon','SpMax_B','Psi_i_A','nN','SM6_B','nArCOOR','nX','TAR']
+
+df['TAR'] = df['TAR'].replace(['RB', 'NRB'], [1, 0])
+df.replace(to_replace='NaN', value=np.nan, regex=True, inplace=True)
+# df.mean(), df.median()
+df.fillna(df.mean(), inplace=True)
+
+
+X = df[[i for i in list(df.columns) if i != 'TAR']]
+y = df['TAR']
+
+
 
 rnd = check_random_state(1)
 
 # set up dataset
-n_samples = 100
-n_features = 300
+n_samples = 1056
+n_features = len(df.columns)-1
 
 # l1 data (only 5 informative features)
-X_1, y_1 = datasets.make_classification(n_samples=n_samples,
-                                        n_features=n_features, n_informative=5,
-                                        random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
 
 # l2 data: non sparse, but less features
 y_2 = np.sign(.5 - rnd.rand(n_samples))
@@ -34,18 +60,18 @@ X_2 += 5 * rnd.randn(n_samples, n_features // 5)
 
 clf_sets = [(LinearSVC(penalty='l1', loss='squared_hinge', dual=False,
                        tol=1e-3),
-             np.logspace(-2.3, -1.3, 10), X_1, y_1),
+             np.logspace(-2.3, -1.3, 10), X_train_std, y_train),
             (LinearSVC(penalty='l2', loss='squared_hinge', dual=True),
-             np.logspace(-4.5, -2, 10), X_2, y_2)]
+             np.logspace(-4.5, -2, 10), X_train_std, y_train)]
 
-colors = ['navy', 'cyan', 'darkorange']
+colors = ['navy', 'cyan', 'darkorange', 'blue', 'green']
 lw = 2
 
 for clf, cs, X, y in clf_sets:
     # set up the plot for each regressor
     fig, axes = plt.subplots(nrows=2, sharey=True, figsize=(9, 10))
 
-    for k, train_size in enumerate(np.linspace(0.3, 0.7, 3)[::-1]):
+    for k, train_size in enumerate(np.linspace(0.30, 0.70, 4)[::-1]):
         param_grid = dict(C=cs)
         # To get nice curve, we need a large number of iterations to
         # reduce the variance
